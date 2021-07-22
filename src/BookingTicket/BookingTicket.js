d2b7f46717dde9386d215d9b67d4ac4657aae257
Hoang <hoangntu56@gmail.com>
2021-07-22 19:35:36 +0700

    first commit

---
+++ src/BookingTicket/BookingTicket.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class BookingTicket extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
